58dd64b4ae6412de737732e517edb6168a07339f
Thomas <thomasmetias29@gmail.com>
2025-09-21 16:57:32 +0300

    refactor(blog-list): add step 2 solution
    - Separate index.js into modules as instructed

---
+++ blog-list/app.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const notesRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', notesRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- blog-list/index.js
+++ blog-list/index.js
@@ -1,18 +1,8 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const notesRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const app = require('./app')
+const logger = require('./utils/logger')
 
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(express.json())
-
-app.use('/api/blogs', notesRouter)
-
-const PORT = process.env.PORT || 3001
+const PORT = config.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
