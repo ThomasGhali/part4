ecf4201ba4c597ca54cb9bcc5228df2d8594a06a
Thomas <thomasmetias29@gmail.com>
2025-09-21 20:05:16 +0300

    feat(blog-list-testing): add step 2 solution
    - add totalLikes function that sums likes in a blog list
    - add tests for the function

--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -2,4 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  const sumOfBlogLikes = (sum, blog) => sum + blog.likes
+
+  return blogs.length === 0 
+    ? 0
+    : blogs.reduce(sumOfBlogLikes, 0)
+}
+
+module.exports = { dummy, totalLikes }
