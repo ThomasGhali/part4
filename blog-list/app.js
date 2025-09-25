1d9362b92825bf9763ea8f462050ba288aba707c
Thomas <thomasmetias29@gmail.com>
2025-09-25 23:45:27 +0300

    feat(blog-list-expansion): complete 4.15 step 3
    - implement POST /api/users endpoint to add users
    - implement GET /api/users endpoint

--- blog-list/app.js
+++ blog-list/app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const notesRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 
 const mongoUrl = config.MONGODB_URI
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', notesRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
