ec8f55ec557a8ffb4447ce82027da473a1b3d751
Thomas <thomasmetias29@gmail.com>
2025-09-26 21:34:14 +0300

    feat(blog-list-expanasion): complete 4.19 step 7
    - refactor POST /api/blogs to require a valid JWT
    - return 401 if token is missing or invalid
    - add case in the errorHandler for invalid token

--- blog-list/controllers/blogs.js
+++ blog-list/controllers/blogs.js
@@ -1,6 +1,8 @@
 const blogsRouter = require('express').Router()
+const config = require('../utils/config')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -13,10 +15,23 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const extractToken = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const user = await User.findOne({})
+  const token = extractToken(request)
+  const decodedToken = jwt.verify(token, config.SECRET)
+
+  if (!decodedToken.id) return response.status(401).json({ error: 'invalid token' })
+
+  const user = await User.findById(decodedToken.id)
 
   if (!user) {
     return response.status(400).json({ error: 'no users found in database' })
